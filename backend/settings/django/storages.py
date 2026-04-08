2801422878b38186351ac1a50feb6c16c787c115
Siavash Outadi <siavash.outadi@gmail.com>
2026-04-08 17:31:42 +0200

    Initial commit

---
+++ backend/settings/django/storages.py
@@ -0,0 +1,13 @@
+from settings.django.base import BASE_DIR
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
