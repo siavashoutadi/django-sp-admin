e144ebb55fde1deb8e22c74ca3e90d5c92318e31
Siavash Outadi <siavash.outadi@gmail.com>
2026-04-15 19:52:45 +0200

    Add admin registration pages

--- backend/urls/django/admin.py
+++ backend/urls/django/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.views import (
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+)
 from django.urls import path
 
-
 urlpatterns = [
+    path(
+        "admin/password_reset/",
+        PasswordResetView.as_view(),
+        name="admin_password_reset",
+    ),
+    path(
+        "admin/password_reset/done/",
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "admin/reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "admin/reset/done/",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("admin/", admin.site.urls),
 ]
